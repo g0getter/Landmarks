c1d45ab042caf28488fa7f3dafb568a13bba6d80
Aiden <nagyeong.yeo1023@gmail.com>
2022-06-17 20:18:54 +0900

    Tutorial2: dynamic list

---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by 여나경 on 2022/06/15.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}
