4ceeef34f7a16d081c1c153f2eaca1c56d6b70a7
Aiden <nagyeong.yeo1023@gmail.com>
2022-06-17 20:18:54 +0900

    Tutorial2: dynamic list

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,24 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by 여나경 on 2022/06/15.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                LandmarkRow(landmark: landmark)
+            }
+        }.navigationTitle("Landmarks")
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
