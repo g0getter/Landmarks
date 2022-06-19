3a25f813420cd8cb3026e743141f7fa386fad88d
Aiden <nagyeong.yeo1023@gmail.com>
2022-06-19 15:01:59 +0900

    struct에 landmark 인자 추가, LandmarkDetail 뷰 수정 등

--- Landmarks/Views/LandmarkDetail.swift
+++ Landmarks/Views/LandmarkDetail.swift
@@ -8,13 +8,53 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    var landmark: Landmark
+    
+//    var body: some View {
+//        LandmarkList()
+//    }
     var body: some View {
-        LandmarkList()
+        ScrollView {
+            MapView(coordinate: landmark.locationCoordinate)
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage(image: landmark.image)
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .padding()
+                    .font(.largeTitle)
+                    .foregroundColor(.blue)
+                HStack {
+                    Text(landmark.park)
+                    Spacer()
+                    Text(landmark.state)
+                    
+                }
+                .font(.subheadline)
+                
+                Divider()
+                
+                Text("About \(landmark.name)")
+                    .font(.headline)
+                Text(landmark.description)
+                    .font(.footnote)
+            }
+            .padding()
+            
+//            Spacer()
+        }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
+        
     }
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkDetail()
+        LandmarkDetail(landmark: landmarks[0])
     }
 }
